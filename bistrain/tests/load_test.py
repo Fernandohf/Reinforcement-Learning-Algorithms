3b5711e4a088b36d37bccaface332bd95f3986c7
Fernando Henrique Fernandes <nandohfernandes@gmail.com>
2019-12-03 18:27:32 -0300

    bug: fix files used on tests

--- bistrain/tests/load_test.py
+++ bistrain/tests/load_test.py
@@ -2,27 +2,29 @@ import os
 import pytest
 from ..load.configuration import BisTrainConfiguration, MissingParameterException, InvalidParameterException
 
+LOCAL_FOLDER = os.path.dirname(__file__)
+
 
 class TestBisTrainConfiguration():
     """
     Basic test for BisTrainConfiguration class
     """
     def test_invalid1(self):
-        file = os.path.join('tests', 'test_invalid_config.yaml')
+        file = os.path.join(LOCAL_FOLDER, 'test_invalid_config_1.yaml')
         with pytest.raises(InvalidParameterException):
             BisTrainConfiguration(file)
 
     def test_invalid2(self):
-        file = os.path.join('tests', 'test_invalid_config.yaml')
+        file = os.path.join(LOCAL_FOLDER, 'test_invalid_config_2.yaml')
         with pytest.raises(KeyError):
             BisTrainConfiguration(file)
 
     def test_valid(self):
-        file = os.path.join('tests', 'test_valid_config.yaml')
+        file = os.path.join(LOCAL_FOLDER, 'test_valid_config.yaml')
         a = BisTrainConfiguration(file)
         assert a.getvalue("AGENT", "action_size") == 2
 
     def test_missing(self):
-        file = os.path.join('tests', 'test_missing_config.yaml')
+        file = os.path.join(LOCAL_FOLDER, 'test_missing_config.yaml')
         with pytest.raises(MissingParameterException):
             BisTrainConfiguration(file)
