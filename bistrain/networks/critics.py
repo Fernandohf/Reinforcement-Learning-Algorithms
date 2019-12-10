28e3565aa2ecf649458c7dd8d04c28301718fbbf
Fernando Henrique <nandohfernandes@gmail.com>
2019-12-10 20:54:25 -0300

    feat:  initialize LSTM critic

--- bistrain/networks/critics.py
+++ bistrain/networks/critics.py
@@ -21,7 +21,7 @@ class FCCritic(nn.Module):
             Dimension of each state
         action_size: int
             Dimension of each action
-        hidden_units: iterable
+        hidden_size: iterable
             Iterable with the hidden units dimensions
         seed: int
             Random seed
@@ -52,3 +52,49 @@ class FCCritic(nn.Module):
         # No activatioin in last layer
         q = self.layers[-1](x)
         return q
+
+
+class LSTMCritic(nn.module):
+    """
+    Basic critic network with LSTM architecture.
+    
+    Receives a sequence of states/actions to output q-value function.
+    """
+    def __init__(self, state_size, action_size, num_layers=2,
+                 hidden_size=128, seed=42, dropout=.1):
+        """
+        Initialize parameters and build model.
+
+        Parameters
+        ----------
+        state_size: int
+            Dimension of each state
+        action_size: int
+            Dimension of each action
+        num_layers: int
+            Number os stacked LSTM layers
+        hidden_size: iterable
+            Iterable with the hidden units dimensions
+        seed: int
+            Random seed
+        dropout: float
+            Probability of Dropout layer between LSTMs
+        """
+        super().__init__()
+        # Set seed
+        self.seed = torch.manual_seed(seed)
+
+        # Model
+        self.lstm = nn.LSTM(state_size + action_size, hidden_size, num_layers,
+                            batch_first=True, dropout=dropout)
+        
+    def forward(self, state, action):
+        """
+        TODO
+        Propagates critic network that maps
+        states and actions pairs to Q-values
+        """
+        x = torch.cat((state, action), dim=1)
+        # TODO
+        
+        return x
