2510d585bb827ce1ffd348d6ba9eed250b8ee0d7
Fernando Henrique <nandohfernandes@gmail.com>
2019-12-13 22:40:42 -0300

    test: add few tests layouts

--- bistrain/tests/agents_test.py
+++ bistrain/tests/agents_test.py
@@ -8,9 +8,12 @@ from torch.optim import Adam
 
 from ..agents import A2CAgent
 from ..utils.noise import GaussianNoise, OUNoise
+from ..networks.actors import FCActorContinuous, FCActorDiscrete
+from ..networks.critics import FCCritic, LSTMCritic
 
 LOCAL_FOLDER = os.path.dirname(__file__)
 VALID_FILE = os.path.join(LOCAL_FOLDER, 'test_valid_config.yaml')
+VALID_FILE_A2C = os.path.join(LOCAL_FOLDER, 'test_valid_a2c.yaml')
 
 
 # TODO
@@ -19,8 +22,8 @@ class TestA2CAgent():
     Test class to A2C Agent
     """
 
-    def _create_agent(self):
-        a = A2CAgent(VALID_FILE)
+    def _create_agent(self, file=VALID_FILE):
+        a = A2CAgent(file)
         return a
 
     def test_optimizer(self):
@@ -30,3 +33,7 @@ class TestA2CAgent():
     def test_noise(self):
         a = self._create_agent()
         assert isinstance(a.noise, GaussianNoise)
+
+    def test_policy(self):
+        a = self._create_agent(VALID_FILE_A2C)
+        assert isinstance(a.actor, FCActorContinuous)
