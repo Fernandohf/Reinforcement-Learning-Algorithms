3eb37ed965c233bee64ee544f9f10a3a1d289f08
Fernando Henrique <nandohfernandes@gmail.com>
2019-12-18 21:09:26 -0300

    feat: add soft update

--- bistrain/utils/experience.py
+++ bistrain/utils/experience.py
@@ -118,3 +118,23 @@ def n_step_boostrap(envs, agent, previous_states, n_bootstrap=5):
                     np.stack(states_next_list, axis=1),
                     np.stack(dones_list, axis=1))
     return trajectories
+
+
+def soft_update(self, local_model, target_model, tau):
+    """
+    Soft update model parameters.
+    θ_target = τ * θ_local + (1 - τ) * θ_target
+
+    Parameters
+    ----------
+    local_model: torch.Module
+        PyTorch model (weights will be copied from)
+    target_model: torch.Module
+        PyTorch model (weights will be copied to)
+    tau: float
+        Interpolation parameter
+    """
+    for target_param, local_param in zip(target_model.parameters(),
+                                         local_model.parameters()):
+        target_param.data.copy_(tau * local_param.data +
+                                (1.0 - tau) * target_param.data)
