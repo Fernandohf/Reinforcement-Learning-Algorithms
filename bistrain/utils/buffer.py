83a993975cd8cde5a2e488a8b7180eb806995799
Fernando Henrique <nandohfernandes@gmail.com>
2019-12-21 20:29:31 -0300

    refactor: update configurations

--- bistrain/utils/buffer.py
+++ bistrain/utils/buffer.py
@@ -11,54 +11,55 @@ class ReplayBuffer:
     """
     Fixed-size buffer to store experience tuples.
     """
-    def __init__(self, buffer_size, batch_size, seed, device):
+    def __init__(self, config):
         """
         Initialize a ReplayBuffer object.
 
         Parameters
         ----------
-            buffer_size :int
-                Maximum size of buffer
-            batch_size: int
-                Size of each training batch
-            seed: int
-                Random seed
+            config : LocalConfig
+                All configuration parameters of the buffer
+                (buffer_size, batch_size, seed, device)
         """
-        self.memory = deque(maxlen=buffer_size)  # internal memory (deque)
-        self.batch_size = batch_size
+        self.config = config
+        self.memory = deque(maxlen=config.BUFFER_SIZE)  # internal memory
         self.experience = namedtuple("Experience",
                                      field_names=["state", "action", "reward",
                                                   "next_state", "done"])
-        self.seed = random.seed(seed)
-        self.device = device
 
     def add(self, state, action, reward, next_state, done):
         """Add a new experience to memory."""
-        # e = self.experience(state, action, reward, next_state, done)
-        # self.memory.append(e)
+        shape_len = len(state.shape)
         # In case of multiple agents
-        for i in range(state.shape[0]):
-            e = self.experience(state[i], action[i], reward[i],
-                                next_state[i], done[i])
+        if shape_len > 2:
+            for i in range(state.shape[0]):
+                e = self.experience(state[i], action[i], reward[i],
+                                    next_state[i], done[i])
+                self.memory.append(e)
+        # Single agent
+        else:
+            e = self.experience(state, action, reward,
+                                next_state, done)
             self.memory.append(e)
 
     def sample(self):
         """
         Randomly sample a batch of experiences from memory
         """
-        exps = random.sample(self.memory, k=self.batch_size)
+        exps = random.sample(self.memory, k=self.config.BATCH_SIZE)
 
-        states = torch.from_numpy(np.vstack([e.state for e in exps
-                                  if e is not None])).float().to(self.device)
-        actions = torch.from_numpy(np.vstack([e.action for e in exps
-                                   if e is not None])).float().to(self.device)
-        rewards = torch.from_numpy(np.vstack([e.reward for e in exps
-                                   if e is not None])).float().to(self.device)
-        n_states = torch.from_numpy(np.vstack([e.next_state for e in exps
-                                    if e is not None])).float().to(self.device)
-        dones = torch.from_numpy(np.vstack([e.done for e in exps
-                                            if e is not None])
-                                 .astype(np.uint8)).float().to(self.device)
+        states = (torch.from_numpy(np.vstack([e.state for e in exps
+                  if e is not None])).float().to(self.config.DEVICE))
+        actions = (torch.from_numpy(np.vstack([e.action for e in exps
+                   if e is not None])).float().to(self.config.DEVICE))
+        rewards = (torch.from_numpy(np.vstack([e.reward for e in exps
+                   if e is not None])).float().to(self.config.DEVICE))
+        n_states = (torch.from_numpy(np.vstack([e.next_state for e in exps
+                    if e is not None])).float().to(self.config.DEVICE))
+        dones = (torch.from_numpy(np.vstack([e.done for e in exps
+                 if e is not None]).astype(np.uint8))
+                 .float().to(self.config.DEVICE))
+
         return (states, actions, rewards, n_states, dones)
 
     def __len__(self):
