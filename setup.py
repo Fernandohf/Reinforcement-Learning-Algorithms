c9469b19c16fa3b64631a97198f0f30234d4766a
Fernando Henrique <nandohfernandes@gmail.com>
2019-12-30 21:37:31 -0300

    chore: add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup
+setup(
+  name = 'bistrain',
+  packages = ['bistrain'],
+  version = '0.1',
+  license = 'MIT',
+  description = 'Simple library of Reinforcement Learning Algorithms\
+                 implemented in Pytorch',
+  author = 'Fernando Henrique',
+  author_email = 'nandohfernandes@gmail.com',
+  url = 'https://github.com/fernandohf/bistrain',
+  download_url = 'https://github.com/fernandohf/bistrain/archive/v_01.tar.gz', # TODO
+  keywords = ['PYTORCH', 'A2C', 'DDPG', 'REINFORCEMENT-LEARNING', 'RL'],
+  install_requires=[
+          'validators',
+          'beautifulsoup4',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers/Researchers',
+    'Topic :: Software Development :: Build Tools::',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
